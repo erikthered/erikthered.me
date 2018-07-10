64aae0f3e46fb181c001d14c01291944f8e2e70d
Erik Nelson <erik.david.nelson@gmail.com>
2018-07-10 14:42:38 -0400

    Add remark transformer plugin and create a welcome post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+export default ({ data }) => {
+  console.log(data)
+  return (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h2>Posts:</h2>
+    {data.allMarkdownRemark.edges.map(({node}) => (
+      <div key={node.id}>
+        <h3>{node.frontmatter.title}{" "}- {node.frontmatter.date}</h3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </div>
-)
+)}
 
-export default IndexPage
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  } 
+`;
---
+++ src/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome"
+date: "2018-07-10"
+---
+
+Welcome to my new site, it's built with Gatsby.
