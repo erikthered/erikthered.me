013fc33ade3dce07e7b305bc0e5b1991000e5504
Erik Nelson <erik.david.nelson@gmail.com>
2018-11-03 15:24:31 -0400

    Replace boundActionCreators with actions

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,8 +7,8 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  const { createNodeField } = boundActionCreators
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode })
     createNodeField({
@@ -19,40 +19,41 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 }
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
   return new Promise((resolve, reject) => {
-
     createPage({
       path: `/resume`,
-      component: path.resolve(`./src/components/resume.js`)
+      component: path.resolve(`./src/components/resume.js`),
     })
 
     graphql(`
-        {
-          allFile(filter:{relativeDirectory:{eq:"posts"}}) {
-            edges {
-              node {
-                childMarkdownRemark {
-                  fields {
-                    slug
-                  }
+      {
+        allFile(filter: { relativeDirectory: { eq: "posts" } }) {
+          edges {
+            node {
+              childMarkdownRemark {
+                fields {
+                  slug
                 }
               }
             }
           }
         }
-        `).then(result => {
-        result.data.allFile.edges.forEach(({ node: { childMarkdownRemark: node } }) => {
+      }
+    `).then(result => {
+      result.data.allFile.edges.forEach(
+        ({ node: { childMarkdownRemark: node } }) => {
           createPage({
             path: node.fields.slug,
             component: path.resolve(`./src/components/blog-post.js`),
             context: {
-              slug: node.fields.slug
+              slug: node.fields.slug,
             },
           })
-        })
-        resolve()
-      })
+        }
+      )
+      resolve()
+    })
   })
 }
