29e52c06164842893c57d74554aca2479744dd9f
Erik Nelson <erik.david.nelson@gmail.com>
2019-03-25 15:59:13 -0400

    Show date in blog post component

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -8,6 +8,7 @@ export default ({ data }) => {
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
+        <h3>{post.frontmatter.date}</h3>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
@@ -20,6 +21,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date
       }
     }
   }
