192a6f31d397d524a40a28d034767bdba5b90abb
Erik Nelson <erik.david.nelson@gmail.com>
2019-04-08 09:42:15 -0400

    Add RSS feed for blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'erikthered.me',
     description: 'Erik Nelson\'s site',
-    keywords: ['blog', 'programming', 'personal site']
+    keywords: ['blog', 'programming', 'personal site'],
+    siteUrl: `https://www.erikthered.me`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -36,5 +37,57 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(filter: {frontmatter:{title: {ne: ""}}}, sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `, // TODO filter this in a more meaningful way
+            output: "/posts/rss.xml",
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      }
+    }
   ],
 }
