0c1080a1199ecea6cf0a4154cafe00eea36e5ec2
Erik Nelson <erik.david.nelson@gmail.com>
2020-02-04 15:43:53 -0500

    Fix formatting of skills section

--- src/components/resume/skillset.js
+++ src/components/resume/skillset.js
@@ -4,9 +4,11 @@ export default ({ skillset }) => {
   return (
     <li className="text-sm italic font-semibold">
       {skillset.name}:
-      <ul className="flex flex-row text-sm">
+      <ul className="flex flex-row text-sm not-italic font-normal mx-0">
         {skillset.keywords.map(skill => (
-          <li className="rounded bg-gray-400 px-2 mx-2 print:px-0">{skill}</li>
+          <li className="rounded bg-gray-400 px-2 mx-2 my-0 print:px-0 text-sm">
+            {skill}
+          </li>
         ))}
       </ul>
     </li>
