4b6a7d2e5edb7647891a3d930edc3b199a0413ca
Erik Nelson <erik.david.nelson@gmail.com>
2020-02-04 16:56:15 -0500

    Add header link to resume

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,15 @@ import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
   <div className="bg-red-900 mb-4 px-2 text-white">
-    <div>
-      <h1 className="text-5xl">
+    <div className="flex flex-row">
+      <h1 className="text-5xl w-2/3">
         <Link to="/">{siteTitle}</Link>
       </h1>
+      <div className="w-1/3 self-center text-right mr-4">
+        <a href="/resume" target="_blank">
+          Resume
+        </a>
+      </div>
     </div>
   </div>
 )
